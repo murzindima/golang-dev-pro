eee43590fd643fa271fe45c81f0fd7d4f2fd41d3
Dmitry Murzin <dmitry.murzin@exness.com>
2022-03-27 02:26:17 +0300

    Unpack string hw02

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,41 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(s string) (string, error) {
+	var unpackedStr strings.Builder
+	unpackedStr.Reset()
+	for i, _rune := range s {
+		if s == "" {
+			return "", nil
+		}
+		if unicode.IsDigit(_rune) && i == 0 {
+			return "", ErrInvalidString
+		}
+		if unicode.IsDigit(_rune) && unicode.IsDigit(rune(s[i-1])) {
+			return "", ErrInvalidString
+		}
+		if i < len(s)-1 {
+			if unicode.IsLetter(_rune) && !unicode.IsDigit(rune(s[i+1])) {
+				unpackedStr.WriteRune(_rune)
+			}
+		}
+		if i == len(s)-1 {
+			if unicode.IsLetter(_rune) && !unicode.IsDigit(rune(s[len(s)-1])) {
+				unpackedStr.WriteRune(_rune)
+			}
+		}
+		if unicode.IsDigit(_rune) {
+			_s := string(s[i-1])
+			_count, _ := strconv.Atoi(string(_rune))
+			unpackedStr.WriteString(strings.Repeat(_s, _count))
+		}
+	}
+	return unpackedStr.String(), nil
 }
