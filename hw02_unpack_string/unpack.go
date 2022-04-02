b3e056620fdec7a1931d9a416e73b628f5181b58
Dmitry Murzin <dmitry.murzin@exness.com>
2022-04-02 20:49:34 +0300

    Fix var naming and err handling

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -12,30 +12,33 @@ var ErrInvalidString = errors.New("invalid string")
 func Unpack(s string) (string, error) {
 	var unpackedStr strings.Builder
 	unpackedStr.Reset()
-	for i, _rune := range s {
+	for i, r := range s {
 		if s == "" {
 			return "", nil
 		}
-		if unicode.IsDigit(_rune) && i == 0 {
+		if unicode.IsDigit(r) && i == 0 {
 			return "", ErrInvalidString
 		}
-		if unicode.IsDigit(_rune) && unicode.IsDigit(rune(s[i-1])) {
+		if unicode.IsDigit(r) && unicode.IsDigit(rune(s[i-1])) {
 			return "", ErrInvalidString
 		}
 		if i < len(s)-1 {
-			if unicode.IsLetter(_rune) && !unicode.IsDigit(rune(s[i+1])) {
-				unpackedStr.WriteRune(_rune)
+			if unicode.IsLetter(r) && !unicode.IsDigit(rune(s[i+1])) {
+				unpackedStr.WriteRune(r)
 			}
 		}
 		if i == len(s)-1 {
-			if unicode.IsLetter(_rune) && !unicode.IsDigit(rune(s[len(s)-1])) {
-				unpackedStr.WriteRune(_rune)
+			if unicode.IsLetter(r) && !unicode.IsDigit(rune(s[len(s)-1])) {
+				unpackedStr.WriteRune(r)
 			}
 		}
-		if unicode.IsDigit(_rune) {
-			_s := string(s[i-1])
-			_count, _ := strconv.Atoi(string(_rune))
-			unpackedStr.WriteString(strings.Repeat(_s, _count))
+		if unicode.IsDigit(r) {
+			s := string(s[i-1])
+			count, err := strconv.Atoi(string(r))
+			if err != nil {
+				return "", err
+			}
+			unpackedStr.WriteString(strings.Repeat(s, count))
 		}
 	}
 	return unpackedStr.String(), nil
