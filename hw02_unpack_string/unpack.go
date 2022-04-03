3055b77cdbed799989ce22c2172464c3f8cafb5c
Dmitry Murzin <dmitry.murzin@exness.com>
2022-04-03 18:55:32 +0300

    Fix rune handling

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -10,36 +10,37 @@ import (
 var ErrInvalidString = errors.New("invalid string")
 
 func Unpack(s string) (string, error) {
-	var unpackedStr strings.Builder
-	unpackedStr.Reset()
-	for i, r := range s {
-		if s == "" {
-			return "", nil
-		}
-		if unicode.IsDigit(r) && i == 0 {
+	rs := []rune(s)
+	var us strings.Builder
+	us.Reset()
+	if len(rs) == 0 {
+		return s, nil
+	}
+	if unicode.IsDigit(rs[0]) {
+		return "", ErrInvalidString
+	}
+	for i, r := range rs {
+		if unicode.IsDigit(r) && unicode.IsDigit(rs[i-1]) {
 			return "", ErrInvalidString
 		}
-		if unicode.IsDigit(r) && unicode.IsDigit(rune(s[i-1])) {
-			return "", ErrInvalidString
-		}
-		if i < len(s)-1 {
-			if unicode.IsLetter(r) && !unicode.IsDigit(rune(s[i+1])) {
-				unpackedStr.WriteRune(r)
+		if i < len(rs)-1 {
+			if !unicode.IsDigit(r) && !unicode.IsDigit(rs[i+1]) {
+				us.WriteRune(r)
 			}
 		}
-		if i == len(s)-1 {
-			if unicode.IsLetter(r) && !unicode.IsDigit(rune(s[len(s)-1])) {
-				unpackedStr.WriteRune(r)
+		if i == len(rs)-1 {
+			if !unicode.IsDigit(r) && !unicode.IsDigit(rs[len(rs)-1]) {
+				us.WriteRune(r)
 			}
 		}
 		if unicode.IsDigit(r) {
-			s := string(s[i-1])
+			s := rs[i-1]
 			count, err := strconv.Atoi(string(r))
 			if err != nil {
 				return "", err
 			}
-			unpackedStr.WriteString(strings.Repeat(s, count))
+			us.WriteString(strings.Repeat(string(s), count))
 		}
 	}
-	return unpackedStr.String(), nil
+	return us.String(), nil
 }
